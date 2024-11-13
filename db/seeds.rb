a177cd311e772cc5bc522b8241d1bad3c517bd24
Carlo <carlodhlln@gmail.com>
2024-11-13 14:41:45 +0100

    sans le refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#   ``
+require 'faker'
+
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Games::LeagueOfLegends.champion,
+    address: Faker::Games::LeagueOfLegends.quote,
+    phone_number:'0685059581',
+    category: 'italian'
+  )
+  restaurant.save!
+end
+puts 'Finished!'
